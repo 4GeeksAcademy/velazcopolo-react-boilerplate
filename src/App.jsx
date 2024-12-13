51aa5e81d6ad881f6709bf7ee10a5b2a4a27ae61
velazcopolo <144776724+velazcopolo@users.noreply.github.com>
2024-12-13 18:18:50 +0000

    router react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   );
 };
